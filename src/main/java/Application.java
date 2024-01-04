94a7236d8c6658d9d28c969e97941cc4740483a3
Prajwal More <prajwal.more@cleartrip.com>
2024-01-05 04:35:30 +0530

    auto

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,16 @@ import io.vertx.core.AbstractVerticle;
 
 public class Application extends AbstractVerticle {
 
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        try {
+            application.start();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     @Override
     public void start() throws Exception {
         super.start();
